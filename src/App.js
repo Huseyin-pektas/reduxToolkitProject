055c0fc9da14a68021d5f6a2ccee097ad8ca8ca9
sefa1-sonmez <pektashuseyin72@gmail.com>
2024-07-07 14:02:39 +0300

    filtreleme ve iş ekleme işlemleri yapıldı. toastify kütüphanesi kuruldu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./component/header";
+import Header from "./component/header"
+import AddJob from "./pages/AddJob"
 import JobList from "./pages/JobList";
-import AddJob from "./pages/addJob";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
     <BrowserRouter >
     <Header/>
+    <ToastContainer />
       <Routes>
         <Route  path="/" element={<JobList/>} />
         <Route  path="/add-job" element={<AddJob/>} />
--- src/pages/addJob.js
+++ src/pages/addJob.js
@@ -1,18 +1,75 @@
-import React from 'react'
+import axios from 'axios';
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const AddJob = () => {
+  const navigate = useNavigate()
 
-const addJob = () => {
+  //  bütün inputları kontrol edeceğim bir state kullanacğım
+
+  const [formState, setFormState] = useState({
+    id: Number(new Date().getTime()),
+    position: "",
+    location: "",
+    company: "",
+    status: "",
+    type: "",
+    date: new Date().toLocaleDateString().replaceAll(".", "/")
+  })
+  //  burada ekle butununa basınca aşağıda  oluşturduğmuz handle
+  // fonsksiyonunu çağırıyoruz
+  const handleSubmit = () => {
+
+    if (!formState.position || !formState.company || !formState.location) {
+      // burada daha güzel alert için toast kütüp. kullandım 
+      toast.error('🦄 Form alanlarını doldurun!', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      return
+    } else {
+      // console.log(formState)
+      toast.success("form başarıyla dolduruldu")
+    }
+
+    //  burada apiye yeni oluşturduğum form göndermem  lazım
+    // veri göderme işi
+    axios.post("http://localhost:3030/jobs", formState)
+
+      .then((res) => {
+        // kullanıcıyı bilgilendir
+        toast.success('İş başarıyla eklendi');
+        // kullanıcıyı ana sayfaya yönlendirir
+        navigate('/');
+      })
+      // .then((res) => {
+      //   toast.success("form başarıyla gönderildi");
+      //   navigate("/")
+      // })
+      .catch((err) => { toast.error("form göderilirken bir hata oluştu") })
+  }
+
+
   return (
+
     <section className="add-sec">
       <h2>Yeni İş Ekle</h2>
-
       <div className="inputs">
         <div className="input-field">
           <label>Pozisyon</label>
           <input
             type="text"
-            // onChange={(e) =>
-            //   setFormState({ ...formState, position: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormState({ ...formState, position: e.target.value })
+            }
           />
         </div>
 
@@ -20,9 +77,9 @@ const addJob = () => {
           <label>Şirket</label>
           <input
             type="text"
-            // onChange={(e) =>
-            //   setFormState({ ...formState, company: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormState({ ...formState, company: e.target.value })
+            }
           />
         </div>
 
@@ -30,18 +87,18 @@ const addJob = () => {
           <label>Lokasyon</label>
           <input
             type="text"
-            // onChange={(e) =>
-            //   setFormState({ ...formState, location: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormState({ ...formState, location: e.target.value })
+            }
           />
         </div>
 
         <div className="input-field">
           <label>Durum</label>
           <select
-            // onChange={(e) =>
-            //   setFormState({ ...formState, status: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormState({ ...formState, status: e.target.value })
+            }
           >
             <option value="Mülakat">Mülakat</option>
             <option value="Devam Ediyor">Devam Ediyor</option>
@@ -52,9 +109,9 @@ const addJob = () => {
         <div className="input-field">
           <label>Tür</label>
           <select
-            // onChange={(e) =>
-            //   setFormState({ ...formState, type: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormState({ ...formState, type: e.target.value })
+            }
           >
             <option value="Tam Zaman">Tam Zaman</option>
             <option value="Yarı Zaman">Yarı Zaman</option>
@@ -63,12 +120,12 @@ const addJob = () => {
           </select>
         </div>
 
-        <button 
-        // onClick={handleSubmit}
+        <button
+          onClick={handleSubmit}
         >Ekle</button>
       </div>
     </section>
   )
 }
 
-export default addJob
+export default AddJob
